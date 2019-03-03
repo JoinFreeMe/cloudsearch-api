71132d5988560fc099f7db41354c9064cd38b085
JoinFreeMe <lewishallam80@gmail.com>
2019-03-03 06:09:08 +0000

    update RUBY api example

--- CloudSearch_RUBY_API/CloudSearch_RUBY_API.rb
+++ CloudSearch_RUBY_API/CloudSearch_RUBY_API.rb
@@ -1,5 +1,20 @@
-require 'rest-client'
+require "net/http"
+require "uri"
 
-post = RestClient.post("https://cloudsearch.cf/api/endpoint.php", {User-Agent: "CloudSearch API bot" domain: "cloudsearch.cf"})
+uri = URI.parse("http://cloudsearch.cf/api/endpoint.php")
 
-post.code
+# Shortcut
+response = Net::HTTP.post_form(uri, {"domain" => "cloudsearch.cf"})
+
+# Full control
+http = Net::HTTP.new(uri.host, uri.port)
+
+request = Net::HTTP::Post.new(uri.request_uri)
+request.set_form_data({"domain" => "cloudsearch.cf"})
+
+# Tweak headers, removing this will default to application/x-www-form-urlencoded
+request["Content-Type"] = "application/json"
+
+response = http.request(request)
+
+puts response.body
